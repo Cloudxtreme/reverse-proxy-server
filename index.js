e9afc8fb41a2daaa0cf25e39b72543b2a2677391
Node on vmnode <node@vmnode.kebler.net>
2015-08-28 18:14:48 -0700

    Update reserve proxy settings

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 var proxy = require('redbird')({port: 80});
 
 // Subdomains, paths, everything just works as expected
-proxy.register("4005.trantor.kebler.net", "http://192.168.0.69:8090");
-proxy.register("4005.kebler.net", "http://192.168.0.79:8080");
+// trantor
+proxy.register("4005.kebler.net", "http://192.168.0.69:8090");
+//vmdebian
+proxy.register("4005.vmdebian.kebler.net", "http://192.168.0.79:8080");
+// vmndoe
+proxy.register("4005.vmnode.kebler.net", "http://192.168.0.67:8080");
